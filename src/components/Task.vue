206879aaaf6ca18382467427eec2653d9b39e21d
paulbonneau <bonneau-paul@hotmail.fr>
2018-02-07 14:41:18 +0100

    modify dataset + add utton edit and modify

---
+++ src/components/Task.vue
@@ -0,0 +1,54 @@
+<template>
+    <tr id="task">
+      <td><input :data-id='id' type="checkbox"></td>
+      <td>{{id}}</td>
+      <td>{{firstName}}</td>
+      <td>{{lastName}}</td>
+      <td>{{mail}}</td>
+      <td>{{phone}}</td>
+      <td>{{content}}</td>
+      <td>
+        <div class="btn btn-info">
+          <i class="fa fa-edit"></i>
+        </div>
+        <div class="btn btn-danger">
+          <i class="fa fa-times"></i>
+        </div>
+      </td>
+    </tr>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props:[
+    'id',
+    'firstName',
+    'lastName',
+    'mail',
+    'phone',
+    'content'
+  ],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style media="screen">
+
+  .btn{
+    border-radius: 2px;
+    padding: 4px;
+    display: inline-block;
+    transition: .4s opacity;
+  }
+
+  .btn:hover{
+    opacity: .6;
+    cursor: pointer;
+  }
+
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="taskList">
+      <h1>{{title}}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>id</th>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+            <th>Contenu</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            <task v-for="task in taskList"
+              :id='task.id'
+              :firstName='task.firstName'
+              :lastName='task.lastName'
+              :mail='task.mail'
+              :phone='task.phone'
+              :content='task.content'
+            ></task>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>#</th>
+            <th>id</th>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>E-mail</th>
+            <th>Phone</th>
+            <th>Contenu</th>
+            <th>Actions</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+import json from '../data/fakeData.json'
+
+export default {
+  name: 'TaskList',
+  components: {Task},
+  data () {
+    return {
+      title: 'Liste des tâches',
+      taskList: json
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
